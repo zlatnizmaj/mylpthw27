7804483d99b4560fa94b24c8e6f92b1732935a50
MrGao <decakgao@gmail.com>
2018-01-27 07:39:34 +0700

    add full ex42.py

--- ex42.py
+++ ex42.py
@@ -2,3 +2,56 @@
 class Animal(object):
     pass
 
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+class Employee(Person):
+    def __init__(self, name, salary):
+        ## ?? hmm What is this strange magic?
+        super(Employee, self).__init__(name)
+        self.salary = salary
+
+class Fish(object):
+    pass
+
+class Salmon(Fish):
+    pass
+
+class Halibut(Fish):
+    pass
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## satan is-a Cat
+satan = Cat("Satan")
+
+## mary is a Person
+mary = Person("Mary")
+
+## Mary has-a pet is Satan
+mary.pet = satan
+
+frank = Employee("Frank", 120000)
+
+frank.pet = rover
+
+flipper = Fish()
+
+crouse = Salmon()
+
+harry = Halibut()
+
+print frank.name, rover.name, frank.salary
